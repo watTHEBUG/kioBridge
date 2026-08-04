ff2deff4c9ce4c2c8cae47e85068d183bbd0c712
parkseyoung <tpdudtpdud128@gmail.com>
2026-08-04 19:13:24 +0900

    feat: initial commit

---
+++ backend/src/main/java/com/kiobridge/kiobridge/KiobridgeApplication.java
@@ -0,0 +1,13 @@
+package com.kiobridge.kiobridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiobridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiobridgeApplication.class, args);
+	}
+
+}
